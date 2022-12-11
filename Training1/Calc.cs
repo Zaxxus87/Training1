efc6eac01b14f0f04abdf90d2fc61c54ff05c85f
Jeffrey Baker <jeffrey1871@gmail.com>
2022-12-11 11:31:45 -0600

    Add project files.

---
+++ Training1/Calc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training1
+{
+    public class Calc
+    {
+        public Calc()
+        {}
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
---
+++ Training1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calc calc1 = new Calc();
+            Console.WriteLine(calc1.Add(2, 3));     //5
+            Console.WriteLine(calc1.Sub(3, 1));     //2
+            Console.WriteLine(calc1.Mult(4, 5));    //20
+            Console.WriteLine(calc1.Div(10, 3));    //3
+
+            int[] vallist = new int[4];
+            vallist[0] = calc1.Add(2, 3);
+            vallist[1] = calc1.Sub(3, 1);
+            vallist[2] = calc1.Mult(4, 2);
+            vallist[3] = calc1.Div(15, 3);
+
+            foreach(int num in vallist)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
